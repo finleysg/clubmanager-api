db584aa0c2459024aca8310a5d213790e8384450
Stuart Finley <finleysg@gmail.com>
2016-11-10 12:54:14 -0600

    update to settings switching logic

--- clubmanager/wsgi.py
+++ clubmanager/wsgi.py
@@ -12,7 +12,7 @@ import os
 import logging_tree
 from django.core.wsgi import get_wsgi_application
 
-settings_file = os.environ.get("DJANGO_SETTINGS_FILE", "clubmanager.settings_development")
+settings_file = os.environ.get("DJANGO_SETTINGS_FILE", "clubmanager.settings")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_file)
 
 application = get_wsgi_application()
