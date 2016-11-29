ee32bed3636b318af0b646b56c66bfcc72acb4c1
Stuart Finley <finleysg@gmail.com>
2016-11-29 08:05:47 -0600

    update slot serializer to include hole id

--- register/serializers.py
+++ register/serializers.py
@@ -5,13 +5,14 @@ from .models import RegistrationGroup, RegistrationSlot
 class RegistrationSlotSerializer(serializers.ModelSerializer):
     course = serializers.CharField(source="course_setup_hole.course_setup.name")
     course_setup_id = serializers.CharField(source="course_setup_hole.course_setup.id")
-    hole = serializers.IntegerField(source="course_setup_hole.hole_number")
+    hole_number = serializers.IntegerField(source="course_setup_hole.hole_number")
+    hole_id = serializers.IntegerField(source="course_setup_hole.id")
     member_first_name = serializers.CharField(source="member.user.first_name")
     member_last_name = serializers.CharField(source="member.user.last_name")
 
     class Meta:
         model = RegistrationSlot
-        fields = ("id", "course", "course_setup_id", "hole", "registration_group",
+        fields = ("id", "course", "course_setup_id", "hole_number", "hole_id", "registration_group",
                   "member", "member_first_name", "member_last_name",
                   "is_event_fee_paid", "is_greens_fee_paid", "is_gross_skins_paid", "is_net_skins_paid",
                   "starting_order", "slot", "status", "expires")
