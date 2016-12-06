787cbf866c3d99910ec637f2e8d92a259b4d9596
Stuart Finley <finleysg@gmail.com>
2016-12-06 07:50:35 -0600

    rework how customer id is passed to stripe

--- register/views.py
+++ register/views.py
@@ -113,6 +113,7 @@ def stripe_charge(user, event, amount_due, token):
 
     stripe.api_key = settings.STRIPE_SECRET_KEY
     member = user.member
+    customer_id = ""
 
     # scenario: member does not have a stripe customer id
     if (member.stripe_customer_id == "" or member.stripe_customer_id is None) and token != "no-token":
@@ -121,6 +122,7 @@ def stripe_charge(user, event, amount_due, token):
             email=user.email,
             source=token
         )
+        customer_id = customer.stripe_id
         member.stripe_customer_id = customer.stripe_id
         member.save()
 
@@ -129,28 +131,28 @@ def stripe_charge(user, event, amount_due, token):
         customer = stripe.Customer.retrieve(id=member.stripe_customer_id)
         customer.source = token
         customer.save()
+        customer_id = customer.stripe_id
 
     # scenario: member has stripe customer id and using existing card (source)
     elif member.stripe_customer_id != "" and member.stripe_customer_id is not None and token == "no-token":
-        pass
+        customer_id = member.stripe_customer_id
 
     # invalid request
     else:
         raise StripePaymentError("Missing stripe id and/or stripe token")
 
-    return create_stripe_charge(user, event, amount_due)
+    return create_stripe_charge(user, customer_id, event, amount_due)
 
 
-def create_stripe_charge(user, event, amount_due):
+def create_stripe_charge(user, customer_id, event, amount_due):
 
     charge_description = "{} ({}): {}".format(event.name, event.get_event_type_display(), event.start_date.strftime('%Y-%m-%d'))
-    member = get_object_or_404(Member, pk=user.id)
 
     try:
         return stripe.Charge.create(
             amount=amount_due,
             currency="usd",
-            customer=member.stripe_customer_id,
+            customer=customer_id,
             receipt_email=user.email,
             description=charge_description,
             metadata={
