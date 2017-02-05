934b2aedb12e5baa583f134b9c1f11ecf062d8d6
Stuart Finley <finleysg@gmail.com>
2017-02-05 08:15:05 -0600

    change to the logging strategery

--- core/exception_handler.py
+++ core/exception_handler.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from rest_framework.views import exception_handler
 from rest_framework.compat import set_rollback
 from rest_framework.response import Response
-from raven.contrib.django.raven_compat.models import client
 
 logger = logging.getLogger(__name__)
 
@@ -14,18 +13,14 @@ def custom_exception_handler(exc, context):
     # Call REST framework's default exception handler first
     # to get the standard error response.
     response = exception_handler(exc, context)
-
-    exc_info = (type(exc), exc, None)
-    client.captureException(exc_info)
+    logger.error(exc, extra={'context': context})
 
     # response == None is an exception not handled by the DRF framework in the call above
     if response is None:
-        logger.error(exc)
-
         if isinstance(exc, IntegrityError):
-            response = Response({"detail": "Must be unique - that one is already being used"}, status=status.HTTP_409_CONFLICT)
+            response = Response({"detail": "Probably a unique index violation"}, status=status.HTTP_409_CONFLICT)
         else:
-            response = Response({'detail': 'Unhandled server error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            response = Response({'detail': 'Doh! Internal server error.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         set_rollback()
 
