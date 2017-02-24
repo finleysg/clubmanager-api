b7e8e82f3f2d0851b5a5b05802b4adc300b7fba8
Stuart Finley <finleysg@gmail.com>
2017-02-24 13:09:38 -0600

    fix document upload storage location

--- documents/models.py
+++ documents/models.py
@@ -14,11 +14,16 @@ DOCUMENT_TYPE_CHOICES = (
 )
 
 
+def document_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/documents/year/<filename>
+    return "documents/{0}/{1}".format(instance.year, filename)
+
+
 class Document(models.Model):
     document_type = models.CharField(verbose_name="Type", choices=DOCUMENT_TYPE_CHOICES, max_length=1, default="R")
     year = models.IntegerField(verbose_name="Golf Season", default=0)
     title = models.CharField(verbose_name="Title", max_length=120)
-    file = models.FileField(verbose_name="File", upload_to="documents/%Y")
+    file = models.FileField(verbose_name="File", upload_to=document_directory_path)
     last_update = models.DateTimeField(auto_now=True)
     event = models.ForeignKey(verbose_name="Event", to=Event, related_name="documents", blank=True, null=True)
 
